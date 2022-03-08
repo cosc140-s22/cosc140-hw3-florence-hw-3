0a33f7d62570bde3b6404db4cfd8bd8471739831
florenceluo <99026236+florenceluo@users.noreply.github.com>
2022-03-08 13:54:08 +0000

    homework 3! done Yay!

--- ghost.py
+++ ghost.py
@@ -15,10 +15,74 @@ def load_wordlist():
             wordlist.append(line.strip().upper())
     return wordlist
 
-def main():
-    words = load_wordlist()
-    print(f"{len(words)} words loaded.")
+  
+def getletter (name):
+    #if input is NOT a single letter of the alphabet
+  while True:
+    letter = input (name + ", please enter a letter.")
+    if len(letter) != 1 or letter == " " or not letter.isalpha(): 
+      # print an error message and tell player to enter another letter
+      print (name + ', the letter you have entered is not valid. Please enter a single letter, no space or illegal characters.')
+      # if it is a valid letter, add letter to ghostword string, then print what the current word is, then leave the loop      
+    else: 
+      return letter.upper()
 
-    # you can start your code here, inside main
+# getletter('rachel')
+
+
+def fullword(ghostword, words):
+  if len(ghostword) > 3 and ghostword in words:
+    return True
+  return False
+ 
+def  startsword (ghostword, words):
+  for line in words:
+    if line.startswith(ghostword):
+      return True
+  return False 
+
+def checklost (ghostword, words):
+  if fullword (ghostword, words) == True or startsword (ghostword, words) == False: 
+    return True
+  return False
+  #true means someone lost
+  #false means keep going, no one lost
+
+
+
+  
+def main():
+  words = load_wordlist()
+  print(f"{len(words)} words loaded.")
+
+  #ask player for their name and then capitalize the first letter of their names
+  playercount = 1 
+  
+  player1 = input (str("Player one, please enter your name."))
+  player1= player1.capitalize()
+  player2 = input (str ("Player two, please enter your name."))
+  player2= player2.capitalize()
+ 
+
+  ghostword= ""
+  playernum = 1 
+  while checklost (ghostword, words) == False:
+    if playernum == 1:
+      letter = getletter (player1)
+      ghostword += letter.upper()
+      print ("Your current word is now: " +
+ghostword)
+      playernum += 1
+    elif playernum == 2:
+      letter = getletter (player2)
+      ghostword += letter.upper()
+      print ("Your current word is now: " +
+ghostword)
+      playernum -= 1
+  print ("Game over! Ghostword is not valid.")
+  
+ 
+
+
 
 main()
--- urlchecker.py
+++ urlchecker.py
@@ -5,10 +5,71 @@
 #######################################################
 
 def urlchecker(url):
+  
+  url = url.strip()
+  
+  # scheme must start w http or https
+  if not url.startswith("http://") and not url.startswith("https://"):
+    return False
+  
+    
+  #there can only be a single #
+  hashcount= url.count('#')
+  if hashcount > 1:
+    return False
 
-    # your code should go here
+  #there can only be a single ?
+  questcount= url.count ('?')
+  if questcount > 1:
+    return False
 
-    return True
+  #if a # is present, must appear before the ? if present
+  #first, use find to determine index position of # and ? 
+  hashpos= url.find('#')
+  questpos= url.find('?')
+  #if pos of hash is greater than pos of question mark, and its not because the question mark dne, then url invalid
+  if hashpos > questpos and questpos != -1:
+    return False 
+
+  #there cannot be any spaces in the URL
+  if " " in url:
+    return False
+
+  #hostname must not be empty
+  before,after = url.split('://') #this line splits up the scheme as before, and the rest of the url as after
+  poscolon = after.find(':') #this line finds the position of the colon if it exists
+  posslash = after.find ('/') #this line finds position of the slash, dont need to specify to look after :// bc using after
+  colcount= after.count(":") #no more than one colon in url
+  if colcount > 1:
+    return False
+  slashcount= after.count("/") 
+  if slashcount < 1: #must be at least one slash, need at least a slash after the hostname
+    return False 
+    
+  if poscolon != -1:   #if the colon exists, then...
+    if poscolon > posslash: #the colon must come before slash
+      return False
+    if after [: poscolon] == "": # the string starting at position 0 of the after string, until the position of the colon 
+      return False     #must not equal "" or none or empty
+  if poscolon == -1:
+    if after [: posslash] == "": #else if the colon doesnt exist, then the hostname is string slice 0 to slash
+      return False   # that string must not be empty
+    
+
+#port, if present, must only be digits; it must be separated from the hostname with a colon (:); no other colons may be present in the url;
+  colcount= after.count(":") #no more than one colon in url after ://
+  if colcount > 1:
+    return False
+  if colcount == 1:
+    host,port = after.split (':')
+    posslash = port.find ('/') 
+    port= port [0:posslash]
+    for char in port [0:posslash]: #each char in port must be digit
+      if not port.isdigit():
+        return False
+
+      
+  return True
 
 
 def testurl():
@@ -37,4 +98,8 @@ def testurl():
             print(f"{url} is not valid, but your function claimed the opposite")
         else:
             print(f"{url} - ok")
+          
+            
+
+testurl()
 
